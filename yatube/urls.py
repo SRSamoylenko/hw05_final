931f041860c3d9625113f87e10cb0155508a85d8
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-04-12 16:54:31 +0300

    commit

--- yatube/urls.py
+++ yatube/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
+from django.conf.urls import handler404, handler500
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import handler404, handler500
 
 handler404 = "posts.views.page_not_found"   # noqa
 handler500 = "posts.views.server_error"     # noqa
@@ -16,5 +16,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
