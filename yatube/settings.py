f3b94419ee74047458c273dae62f52a2a7cfdd9d
Sergey Samoylenko <s.r.samoylenko@gmail.com>
2021-08-25 03:05:59 +0300

    requirements updated

--- yatube/settings.py
+++ yatube/settings.py
@@ -1,12 +1,8 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django_insecure'
 
 DEBUG = True
 
